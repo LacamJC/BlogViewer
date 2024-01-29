4006e8f8314b2ffd09875683fff88aec69382e5a
LacamJC <joaoramajo744@gmail.com>
2024-01-29 19:37:05 -0300

    Estilizando coisas

--- database/conexao.js
+++ database/conexao.js
@@ -1,18 +1,18 @@
-// const Sequelize = require('sequelize');
-
-// const sequelize = new Sequelize('Blogin', 'ramajo', '',{
-//     dialect: 'mysql',
-//     host: 'localhost', 
-//     port: 3306
-// })
-
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('railway', 'root', 'hcDHBABhcFE-B3-G1eAEHfd1dH-aghGg',{
+const sequelize = new Sequelize('Blogin', 'ramajo', '',{
     dialect: 'mysql',
-    host: 'roundhouse.proxy.rlwy.net', 
-    port: 19983
+    host: 'localhost', 
+    port: 3306
 })
+
+// const Sequelize = require('sequelize');
+
+// const sequelize = new Sequelize('railway', 'root', 'hcDHBABhcFE-B3-G1eAEHfd1dH-aghGg',{
+//     dialect: 'mysql',
+//     host: 'roundhouse.proxy.rlwy.net', 
+//     port: 19983
+// })
 
 module.exports = sequelize
 
